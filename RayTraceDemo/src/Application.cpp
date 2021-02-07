2ba42dd119813cd0451718bf130053d8255c4d7d
luoxiao <515360208@qq.com>
2021-02-07 18:44:48 +0800

    first commit

---
+++ RayTraceDemo/src/Application.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello Ray Tracing" << std::endl;
+
+
+	std::cin.get();
+}
---
+++ premake5.lua
@@ -0,0 +1,31 @@
+workspace "RayTracing"
+	architecture "x64"
+	configurations 
+	{
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "RayTraceDemo"
+	location "RayTraceDemo"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	filter {"configurations:Debug"}
+		defines {"DEBUG"}
+		symbols "On"
+
+	filter {"configurations:Release"}
+		defines {"NDEBUG"}
+		optimize "On"
